acc757913f043f1910d2daea856cebc490943a9b
Hugo Corrales <hhcorrales@gmail.com>
2024-08-10 16:45:38 -0600

    update

--- Act-700/Tarea 08-08-2024.py
+++ Act-700/Tarea 08-08-2024.py
@@ -24,3 +24,37 @@ def factorization(N):
         p = primes[i]
         n = n//p
     return factorization
+
+def compactification(L):
+    if len(L) ==0:
+        return []
+    i = 0
+    j = len(L)
+    compact_list = []
+    L.append(-1)
+    while i<j:
+        e = L[i]
+        compact_list.append([e,L.count(e)])
+        i+=1
+        while e == L[i] and i<j:
+            i+=1
+    return compact_list
+
+def periodo_maximo(N):
+    primes_list_powers = compactification(factorization(N))
+    power_primes_factors = []
+    other_factors =[]
+    for [i,n] in primes_list_powers:
+        if i==0:
+            if n==2:
+                power_primes_factors.append([0,1])
+            else:
+                power_primes_factors.append([0,n-2])
+        else:
+            if n==1:
+                power_primes_factors.append([i,1])
+            else:
+                power_primes_factors.append([i,n-1])
+                other_factors.append(primes[i]-1)
+    print(power_primes_factors)
+    print(other_factors)
