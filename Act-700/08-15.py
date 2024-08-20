5cb677a46be0b73f4660215c6f976c75640418ab
DRHHugo <101353172+DRHHugo@users.noreply.github.com>
2024-08-19 20:05:38 -0600

    update

--- Act-700/08-15.py
+++ Act-700/08-15.py
@@ -10,10 +10,15 @@ def PRMCG(m:int,b:int,x:int,n:int):
         sample.append(x:=b*x%m)
     return sample
 
-def evaluate_pol(coef,x:int)
-    
-
-def PRMCG(m:int,b:int,c:int,x:int,n:int):
+def PRLCG(m:int,b:int,c:int,x:int,n:int):
     b=b%m
     x=x/m
     c=c%m
+    if n<0:
+        return None
+    if n==0:
+        rerurn []|
+    sample = []
+    while len(sample)<n:
+        sample.append(x:=(b*x+c)%m)
+    return sample
