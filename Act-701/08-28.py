a97d07f5ceb4bef8ad97c9649055dcb1ad91f75c
DRHHugo <101353172+DRHHugo@users.noreply.github.com>
2024-08-28 16:57:53 -0600

    update

---
+++ Act-701/08-28.py
@@ -0,0 +1,19 @@
+def num_to_base_2(n:int)-> list[bool]:
+    if n<0:
+        return []
+    if n==0:
+        return [False]
+    if n==1:
+        return [True]
+    bit = False if n%2==0 else True
+    rest = num_to_base_2(n//2)
+    rest.insert(0,bit)
+    return rest
+
+def base_2_to_num(l:list[bool])->int|None:
+    if len(l)==0:
+        return None
+    if len(l)==1:
+        return int(l[0])
+    else:
+        return l[0]+2*base_2_to_num(l[1:len(l)])
