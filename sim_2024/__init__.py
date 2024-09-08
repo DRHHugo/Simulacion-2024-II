880ab7936fc38cc7fbef53ae4bdf38bd5c02a87f
DRHHugo <hhcorrales@gmail.com>
2024-09-08 11:04:24 -0600

    update

--- sim_2024/__init__.py
+++ sim_2024/__init__.py
@@ -1,11 +1,10 @@
 #from exceptions import KeyError,TypeError,ValueError
 from math import floor
 from matplotlib import pyplot
+from warnings import warn
 
 class package_type_warning(UserWarning):
     """package type warning"""
-    def __init__(self,message):
-        self.args=(message)
-        
+    pass    
 
-__all__ = ['floor', 'pyplot','package_type_warning']
+__all__ = ['floor', 'pyplot','package_type_warning','warn']
--- sim_2024/generators.py
+++ sim_2024/generators.py
@@ -34,6 +34,27 @@ def _validate_cte(kwargs:dict)->bool:
         raise TypeError('cte should be an integer')
     return True if kwargs['cte']!=0 else False
 
+def _validate_poly_array(kwargs:dict,exclude_last_zero=False)->bool:
+    if not 'mult' in kwargs.keys():
+        raise KeyError('mult not found during inicialization. You should use cte=list[values]')
+    if type(kwargs['mult'])!=list:
+        raise TypeError('mult should be a non empty list of integers')
+    num_of_zeros = 0
+    for x in kwargs['mult']:
+        if type(x)!=int:
+            listt = kwargs['mult']
+            raise TypeError(f'mult[{listt.index(x)}] should be a integer')
+        if x==0:
+            num_of_zeros+=1
+    if num_of_zeros == len(kwargs['mult']):
+        raise ValueError('mult can\'t be full of zeros')
+    if kwargs['mult'][0]==0 and exclude_last_zero:
+        raise ValueError('seed and mult[0] can\'t be both zeros')
+    return True
+
+def _validate_mult_array(kwargs:dict)->bool:
+    pass
+
 class _random_generator:
     """parent class for random generators"""
     def __str__(self):
@@ -46,7 +67,7 @@ class _congruential_generator(_random_generator):
     _main_type = 'congruential'
 
 class multiplicative_congruential_generator(_congruential_generator):
-    """class for conrguential multiplicative pseudorandom generators"""
+    """class for congruential multiplicative pseudorandom generators"""
     _sub_type = 'multiplicative'
     def __new__(cls,**kwargs):
         _validate_mod(kwargs)
@@ -61,7 +82,7 @@ class multiplicative_congruential_generator(_congruential_generator):
     def rand(self)->float:
         self._state = self._mult*self._state % self._mod
         return self._state/self._mod
-    def sample(self,size:int)->list[float]|None:
+    def sample(self,size:int=1)->list[float]|None:
         if size<0:
             return None
         if size==0:
@@ -74,7 +95,7 @@ class multiplicative_congruential_generator(_congruential_generator):
         return sample
 
 class linear_congruential_generator(_congruential_generator):
-    """class for linear multiplicative pseudorandom generators"""
+    """class for congruential linear pseudorandom generators"""
     _sub_type = 'linear'
     def __new__(cls,**kwargs):
         _validate_mod(kwargs)
@@ -82,7 +103,7 @@ class linear_congruential_generator(_congruential_generator):
         if _validate_cte(kwargs):
             return super().__new__(cls)
         else:
-            package_type_warning('since cte=0 you will get a multiplicative congruential generator instead of a linear congruential generator')
+            warn('since cte=0 you will get a multiplicative congruential generator instead of a linear congruential generator',category=package_type_warning)
             return multiplicative_congruential_generator(mod=kwargs['mod'],mult=kwargs['mult'],seed=kwargs['seed'])
     def __init__(self,**kwargs):
         if type(self!='linear_congruential_generator'):
@@ -94,14 +115,66 @@ class linear_congruential_generator(_congruential_generator):
     def rand(self)->float:
         self._state = (self._mult*self._state + self._cte)%self._mod
         return self._state/self._mod
-    def sample(self,size:int)->list[float]|None:
+    def sample(self,size:int=1)->list[float]|None:
+        if size<0:
+            return None
+        if size==0:
+            return []
+        sample:list[float] = []
+        while len(sample)<size:
+            self._state = (self._mult*self._state + self._cte)%self._mod
+            if self._state!=0:
+                sample.append(self._state/self._mod)
+        return sample
+
+class quadratic_congrential_generator(_congruential_generator):
+    """class for congruential quadratic pseudorandom generators"""
+    _sub_type = 'quadratic'
+    def __new__(cls,**kwargs):
+        _validate_mod(kwargs)
+        _validate_seed(kwargs,exclude_zero=True)
+        return super().__new__(cls)
+    def __init__(self,**kwargs):
+        self._mod = kwargs['mod']
+        self._state = kwargs['seed']%self._mod
+    @property
+    def rand(self)->float:
+        self._state = self._state**2%self._mod
+        return self._state/self._mod
+    def sample(self,size:int=1)->list[float]|None:
         if size<0:
             return None
         if size==0:
             return []
         sample:list[float] = []
         while len(sample)<size and self._state!=0:
-            self._state = (self._mult*self._state + self._cte)%self._mod
+            self._state = self._state**2%self._mod
             if self._state!=0:
                 sample.append(self._state/self._mod)
         return sample
+
+class polynomial_confruential_generator(_congruential_generator):
+    """class for congruential polynomial pseudorandom generators"""
+    _sub_type = 'polynomial'
+    def __new__(cls,**kwargs):
+        _validate_mod(kwargs)
+        _validate_seed(kwargs)
+        _validate_poly_array(kwargs,exclude_last_zero=True if kwargs['seed']==0 else False)
+        return super().__new__(cls)
+    def __init__(self,**kwargs):
+        self._mod = kwargs['mod']
+        self._mult = [b%self._mod for b in kwargs['mult']]
+        while self._mult[-1]==0:
+            self._mult.pop()
+        self._state = kwargs['seed']%self._mod
+    @property
+    def rand(self)->float:
+        x = 0
+        pow = 1
+        for k in range(len(self._mult)):
+            x = (x+pow*self._mult[k])%self._mod
+            pow = pow*self._state%self._mod
+        self._state = x
+        return self._state/self._mod
+    def sample(self,size:int=1)->list[float]:
+        pass
