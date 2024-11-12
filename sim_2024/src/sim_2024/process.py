6e8b954cd86d524c53dd29df9309e7f72d19946e
DRHHugo <101353172+DRHHugo@users.noreply.github.com>
2024-11-12 17:13:27 -0600

    update process

--- sim_2024/src/sim_2024/process.py
+++ sim_2024/src/sim_2024/process.py
@@ -87,7 +87,53 @@ class PoissonProcess(_random_process):
             X.append(arrivals)
             t+=self._exponential.rand()
             arrivals+=1
-        times.append(horizon)
-        X.append(arrivals-1)
         return _process_path(times,X,self._type_paths,self._auto_valuation)
 
+class CompundPoissonProcess(_random_process):
+    """Homogeneous Poisson process """
+    _main_type = 'Compound Poisson'
+    _type_paths = 'jump'
+    _auto_valuation = True
+    def __new__(cls,**kwargs):
+        if 'rate' in kwargs:
+            if type(kwargs['rate'])==float:
+                if kwargs['rate']<=0:
+                    raise ValueError('rate must be a positive float')
+            else:
+                raise TypeError('rate must be a positive float')
+        else:
+            raise KeyError('key argument rate not found during initialization')
+        if 'var' in kwargs:
+            if type(kwargs['var'])==float:
+                if kwargs['var']<=0:
+                    raise ValueError('var must be a positive float')
+            else:
+                raise TypeError('var be a positive float')
+        return super().__new__(cls)
+
+    def __init__(self,**kwargs):
+        if 'var' in kwargs:
+            self._stdev = kwargs['var']**0.5
+        else:
+            self._stdev = 1.
+        self._normal_std = _NormalStd()
+        self._exponential = _Exponential(rate=kwargs['rate'])
+    
+    def rand(self,**kwargs)->_process_path:
+        times:_array
+        X:_array
+        t:float
+        Z_value:float
+        horizon = kwargs['horizon'] if 'horizon' in kwargs else 1.0
+        times = _array('d')
+        X = _array('d')
+        times.append(0.0)
+        X.append(0.0)
+        t = self._exponential.rand()
+        Z_value = self._stdev*self._normal_std.rand()
+        while t<horizon:
+            times.append(t)
+            X.append(Z_value)
+            t+=self._exponential.rand()
+            Z_value+=self._stdev*self._normal_std.rand()
+        return _process_path(times,X,self._type_paths,self._auto_valuation)
